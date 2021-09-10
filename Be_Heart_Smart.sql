9bedb7e8baa4ceacf1c02f45b85ee1d78ec01128
ayseozgun <aorman@gmail.com>
2021-09-10 15:53:26 -0500

    cleaning up the repo

---
+++ Be_Heart_Smart.sql
@@ -0,0 +1,33 @@
+CREATE TABLE cardio_info (
+  id numeric,
+  age numeric,
+  gender numeric,
+  height numeric,
+  weight numeric,
+  ap_hi numeric,
+  ap_lo numeric,
+  cholesterol numeric,
+  gluc numeric,
+  smoke numeric,
+  alco numeric,
+  active numeric,
+  cardio numeric
+);
+
+CREATE TABLE cardio_info_initial_clean (
+  id numeric,
+  age numeric,
+  gender numeric,
+  height numeric,
+  weight numeric,
+  ap_hi numeric,
+  ap_lo numeric,
+  cholesterol numeric,
+  gluc numeric,
+  smoke numeric,
+  alco numeric,
+  active numeric,
+  cardio numeric
+);
+ 
+ Select * From cardio_info_initial_clean;
